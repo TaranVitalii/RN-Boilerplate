837b3aa7a6388e351a2cc13693eb79fec8db8b47
Vitalii Taran <47290832+TaranVitalii@users.noreply.github.com>
2024-05-23 17:36:56 +0200

    Prettier and ESlint (#1)
    * first commit
    
    * add the ESlint and Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,139 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: [
+    '@react-native',
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@react-native-community',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  plugins: [
+    'jest',
+    'react',
+    'react-native',
+    '@typescript-eslint',
+    'module-resolver',
+    'unused-imports',
+  ],
+  rules: {
+    'react-native/no-inline-styles': 0,
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'import/no-extraneous-dependencies': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/destructuring-assignment': [
+      'error',
+      'always',
+      {
+        ignoreClassFields: true,
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-unused-expressions': [
+      'error',
+      {
+        allowTernary: true,
+      },
+    ],
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/state-in-constructor': 0,
+    'react/no-unused-prop-types': 'warn',
+    'default-param-last': 0,
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1,
+        maxBOF: 0,
+        maxEOF: 1,
+      },
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+    'react/function-component-definition': 'off',
+    'no-restricted-exports': 'off',
+    'react/require-default-props': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    semi: [2, 'always'],
+    'eol-last': ['error', 'always'],
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+      },
+    ],
+    '@typescript-eslint/default-param-last': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-shadow': [
+      'error',
+      {
+        ignoreTypeValueShadow: true,
+        ignoreFunctionTypeParameterNameValueShadow: true,
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external'], 'internal', 'parent', 'sibling', 'index', 'object'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'import/no-cycle': ['error', { maxDepth: 10, ignoreExternal: true }],
+  },
+  overrides: [
+    {
+      files: ['src/**/*.spec.ts', 'src/**/*.spec.tsx', 'src/**/*.d.ts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': ['off'],
+      },
+    },
+  ],
 };
